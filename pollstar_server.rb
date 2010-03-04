1e317057d7e6659e34904882d639b8e205fb54e7
Sean Schulte <sirsean@gmail.com>
2010-03-04 07:47:02 -1000

    Accepting Amazon Simple Payments!

--- pollstar_server.rb
+++ pollstar_server.rb
@@ -22,6 +22,8 @@ before do
         puts "Authenticated session from #{@env["REMOTE_ADDR"]}: #{session["user_id"]}"
         @current_user = User.find(session["user_id"])
 
+        @flash = session.delete("flash")
+
         # build the sidebar info
         @sidebar = {
             "username" => @current_user.username,
@@ -75,16 +77,6 @@ post '/signup/?' do
         @errors << "passwords must match"
     end
 
-    if not params["account_level"]
-        account_level = :free
-    else
-        account_level = params["account_level"].to_sym
-    end
-
-    if not [:free, :cheapo, :standard, :deluxe].include?(account_level)
-        @errors << "invalid account level"
-    end
-
     if not @errors.empty?
         return haml :signup
     end
@@ -94,26 +86,70 @@ post '/signup/?' do
         :email => params["email"],
         :password => params["password1"],
         :full_name => params["full_name"],
-        :account_level => account_level,
-        :monthly_rate => User.get_monthly_rate_by_account_level(account_level),
+        :account_level => :free,
+        :monthly_rate => 0,
     })
     @user.save
 
     session["user_id"] = @user.id
+    redirect "/select_plan/"
+end
+
+get '/select_plan/?' do
+    @cheapo_plan_button = config["cheapo_plan_button"]
+    @standard_plan_button = config["standard_plan_button"]
+    @deluxe_plan_button = config["deluxe_plan_button"]
+    haml :select_plan
+end
+
+get '/confirm/?' do
+    puts "Payment confirmation"
+    puts @env["rack.request.query_hash"].inspect
+    reason = @env["rack.request.query_hash"]["paymentReason"]
+
+    account_level = :free
+    case reason
+        when "poll4.me Cheapo Plan"
+            account_level = :cheapo
+        when "poll4.me Standard Plan"
+            account_level = :standard
+        when "poll4.me Deluxe Plan"
+            account_level = :deluxe
+    end
+
+    @current_user.account_level = account_level
+    @current_user.monthly_rate = User.get_monthly_rate_by_account_level(account_level)
+    @current_user.save
+
+    if [:cheapo, :standard, :deluxe].include?(account_level)
+        session["flash"] = "Congratulations! You have signed up for the #{account_level} plan!"
+    end
     redirect_url = (session.delete("redirect_url") or "/home/")
     redirect redirect_url
 end
 
-get '/confirm/' do
-    "Congratulations! You've successfully paid me!"
-end
-
 get '/abandon/' do
-    "I'm sorry you didn't want to pay. You can continue with the free plan."
+    puts "Abandoning payment"
+
+    @current_user.account_level = :free
+    @current_user.monthly_rate = 0
+    @current_user.save
+
+    session["flash"] = "You didn't select a plan, so you can continue on the free plan"
+    redirect_url = (session.delete("redirect_url") or "/home/")
+    redirect redirect_url
 end
 
 get '/cancel/' do
-    "I'm sorry you didn't find the service valuable enough. If you have feedback we can use to make poll4.me better, I'd love to hear about it! Meanwhile, you can continue with the free plan."
+    puts "Cancelling plan"
+
+    @current_user.account_level = :free
+    @current_user.monthly_rate = 0
+    @current_user.save
+
+    session["flash"] = "Sorry you didn't find the service valuable enough. We'd love to hear your feedback to make poll4.me better. Meanwhile, you can continue with the free plan."
+    redirect_url = (session.delete("redirect_url") or "/home/")
+    redirect redirect_url
 end
 
 get '/login/?' do
